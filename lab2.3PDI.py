43e85e2ad11e7408c649f5a51cfb1c18840158e1
aldomellado <aldomellado@udec.cl>
2016-04-23 00:39:07 -0300

    Update lab2.3PDI.py

--- lab2.3PDI.py
+++ lab2.3PDI.py
@@ -2,16 +2,15 @@
 
 from SimpleCV import Camera, Display, Image
 import pylab as plt
-import time
 
 c = Camera()
 img = c.getImage()
 img.save("PDI-image-n.png")
 img.show()
-imgGray = img.grayscale()
-imgGray.save("grayImage.jpg")
-imgGray.show()
-hist = imgGray.histogram()
-plt.plot(hist)
+imgGray = img.grayscale()  %transforma la imagen a escala de grises
+imgGray.save("grayImage.jpg") %guarda la nueva imagen
+imgGray.show() %muestra la nueva imagen
+hist = imgGray.histogram() %hace un histograma con los valores de intensidad por pixel de la imagen recien generada
+plt.plot(hist) %%muestra la imagen del histograma
 
 
