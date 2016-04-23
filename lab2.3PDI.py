5784f5a5b2b126d2a8cdb199535608f3dcef2cc8
aldomellado <aldomellado@udec.cl>
2016-04-23 00:29:42 -0300

    El código genera histograma, pero muestra las imágenes en un solo cuadro

--- lab2.3PDI.py
+++ lab2.3PDI.py
@@ -2,15 +2,16 @@
 
 from SimpleCV import Camera, Display, Image
 import pylab as plt
+import time
 
 c = Camera()
 img = c.getImage()
 img.save("PDI-image-n.png")
 img.show()
-
 imgGray = img.grayscale()
 imgGray.save("grayImage.jpg")
 imgGray.show()
 hist = imgGray.histogram()
 plt.plot(hist)
+
 
